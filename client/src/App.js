335447fd5ec5fc22039d8017c090120fb83505e0
Tim Myers <tim@denvercoder.com>
2018-08-02 01:50:51 -0600

    add redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css'
+import { Provider } from 'react-redux'
+import store from './store'
 
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
@@ -8,20 +9,24 @@ import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 
+import './App.css'
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store} key="provider">
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     )
   }
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import classnames from 'classnames'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { registerUser } from '../../actions/authActions'
+import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 
 class Register extends Component {
   constructor() {
@@ -18,6 +21,12 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this)
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors })
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value })
   }
@@ -32,12 +41,7 @@ class Register extends Component {
       password2: this.state.password2,
     }
 
-    console.log('User', newUser)
-
-    axios
-      .post('/api/users/register', newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }))
+    this.props.registerUser(newUser, this.props.history)
   }
 
   render() {
@@ -133,4 +137,18 @@ class Register extends Component {
   }
 }
 
-export default Register
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors,
+})
+
+export default connect(
+  mapStateToProps,
+  { registerUser },
+)(withRouter(Register))
