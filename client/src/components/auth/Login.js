122bb717a9db2bcdf8ff9adc1ff5b31277e807a6
= <=>
2018-08-20 03:38:55 -0600

    change title

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -57,7 +57,7 @@ class Login extends Component {
               <div className="col-md-8 m-auto">
                 <h1 className="display-4 text-center">Log In</h1>
                 <p className="lead text-center">
-                  Sign in to your DevConnector account
+                  Sign in to your Screentegrity account
                 </p>
                 <form onSubmit={this.onSubmit}>
                   <TextFieldGroup
