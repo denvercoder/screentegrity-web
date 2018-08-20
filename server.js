1882530d6c8504f1c3a17307155f3956f64039c8
= <=>
2018-08-20 02:05:07 -0600

    push to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
@@ -35,6 +36,16 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
+
+// Serve Static Assets if Production
+if (process.env.NODE_ENV === 'production') {
+  // SET STATIC FOLDER
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
