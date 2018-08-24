61c50bfe3770a54198ec2bdcf14208ee81038dfc
= <=>
2018-08-24 10:07:11 -0600

    testing

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class PostItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default PostItem
